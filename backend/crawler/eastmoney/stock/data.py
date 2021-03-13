8163ccd60626c7ec6b1ffa50b6a1ef309c91d534
xingweidong <xingweidong@redoop.com>
2021-03-13 16:07:53 +0800

    增加selenium访问网页

--- backend/crawler/eastmoney/stock/data.py
+++ backend/crawler/eastmoney/stock/data.py
@@ -1,5 +1,23 @@
 '''
-股票概况
+股票数据
+http://data.eastmoney.com/stockdata/600031.html
+
 '''
 
-# http://data.eastmoney.com/stockdata/603993.html
+from selenium.webdriver import Chrome, ChromeOptions
+from bs4 import BeautifulSoup
+
+
+# chromedriver下载地址 https://chromedriver.storage.googleapis.com/index.html
+# chromedriver版本需要和本地chrome浏览器版本保持大版本一致。
+chrome_driver = '/Users/xingweidong/envs/selenium/webdriver/chrome/88.0.4324.96/chromedriver'
+chrome_options = ChromeOptions()
+chrome_options.add_argument('headless')  # 启用静默模式，不打开浏览器窗口
+url = 'http://data.eastmoney.com/stockdata/{}.html'.format('600031')
+with Chrome(executable_path=chrome_driver, options=chrome_options) as driver:
+    driver.get(url)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'lxml')
+    tags = soup.find('div', 'hxtc-content').find_all('p')
+    for p in tags:
+        print(p.b)
